53aa23cfcec6ba3f4a7273d3b52ad2c1c2e59d49
Kawser Ahmed <32969466+kawserahmed47@users.noreply.github.com>
2022-01-29 23:09:07 +0600

    Material Update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -9,19 +9,19 @@ class DatabaseSeeder extends Seeder
 
     public function run()
     {
-        $this->call([UserTypeSeeder::class]);
-        $this->call([RoleSeeder::class]);
-        $this->call([UsersTableSeeder::class]);
-        // $this->call([StudentClassSeeder::class]);
-        // $this->call([StudentYearSeeder::class]);
-        // $this->call([StudentGroupSeeder::class]);
-        // $this->call([StudentShiftSeeder::class]);
-        // $this->call([FeeCategorySeeder::class]);
-        // $this->call([FeeCategoryAmountSeeder::class]);
-        // $this->call([ExamTypeSeeder::class]);
-        // $this->call([SubjectSeeder::class]);
-        // $this->call([DesignationSeeder::class]);
-        // $this->call([AssignSubjectSeeder::class]);
+        // $this->call([UserTypeSeeder::class]);
+        // $this->call([RoleSeeder::class]);
+        // $this->call([UsersTableSeeder::class]);
+        $this->call([StudentClassSeeder::class]);
+        $this->call([StudentYearSeeder::class]);
+        $this->call([StudentGroupSeeder::class]);
+        $this->call([StudentShiftSeeder::class]);
+        $this->call([FeeCategorySeeder::class]);
+        $this->call([FeeCategoryAmountSeeder::class]);
+        $this->call([ExamTypeSeeder::class]);
+        $this->call([SubjectSeeder::class]);
+        $this->call([DesignationSeeder::class]);
+        $this->call([AssignSubjectSeeder::class]);
 
 
     }
